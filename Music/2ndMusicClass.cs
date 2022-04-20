a60d3111e49a0d744b0784ef3a4df4357c50edd6
Melissa Tapia <mtapia.dpl@gmail.com>
2022-04-20 15:04:35 -0500

    Working with classes; nothing works yet

---
+++ Music/2ndMusicClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Music
+{
+            //Four pieces of data for each obj from txt file
+            //Artist, Song Title, Run Time, Album
+    public class Music
+    {
+        string name {get; set;}
+        string song {get; set;}
+        double time {get; set;}
+        string album {get; set;}
+
+        public Music(string name, string song, double time, string album) //Constructor will have same name as class
+        {
+            this.name = name;
+            this.song = song;
+            this.time = time;
+            this.album = album;
+        }
+        
+        public override string ToString()
+        {
+            return "Artist: " + name + "\nSong Title: " + song + "\nRun Time: " + time + "\nAlbum Name: " + album;
+        }
+
+    }
+}
+
---
+++ Music/MainProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Music
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string[] musicInfoArray = new String[80];
+
+            Console.WriteLine("\n    MUSIC INFORMATION ");
+            Console.Write("------------------------------\n\n"); 
+
+            string readText = File.ReadAllText("music.txt");  // Test to read the txt file contents
+            Console.WriteLine(readText);
+
+            for (int index = 0; index < musicInfoArray.Length; index++)
+            {
+                musicInfoArray[index] = new UpMusicInfo();
+            }     
+                
+        }
+    }
+}
