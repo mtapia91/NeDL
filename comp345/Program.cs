d676fac680488e484ae50a58ef5b3afce16f894b
Melissa Tapia <mtapia.dpl@gmail.com>
2022-04-22 14:50:29 -0500

    array test worked

--- comp345/Program.cs
+++ comp345/Program.cs
@@ -6,18 +6,28 @@ namespace comp345
     {
         static void Main(string[] args)
         {
-            /* Employee[] employeeBonusArray=new Employee[50];  // array of employee objs are declared and instantiated
+            Employee[] employeeBonusArray=new Employee[50];  // array of employee objs are declared and instantiated
 
-            for (int index = 0; index < employeeBonusArray.Length; index++)  // testing arrays but stuck
-            {
-            }
-                Employee[index] = new Employee(); 
-
-                    employeeBonusArray[2].setFirstName ("Lisa");
-                    employeeBonusArray[2].setLastName ("Frank");
-                    employeeBonusArray[2].setEmployeeType("Salary"); */
-
+            /* Employee emp1 = new Employee("Frank", "Lisa", "salary");  //testing employee class || it worked
+            Console.WriteLine(emp1); */
             
+            HourlyEmployee emp2 = new HourlyEmployee("Joe", "Frank", "h", 50.25); //testing hourly class
+/*             Console.WriteLine(emp2);  */
+
+            SalaryEmployee emp3 = new SalaryEmployee("Lisa", "Frank", "s", 83654);
+/*             Console.WriteLine(emp3); */
+
+            employeeBonusArray[1] = emp3;
+            employeeBonusArray[3] = emp2;
+
+            for (int index = 0; index < employeeBonusArray.Length; index++)
+            {
+                if (!(employeeBonusArray[index]==null))
+                {
+                    Console.WriteLine (employeeBonusArray[index]);
+                }
+            }
+
         }
 
     }
