430fe78890bc1a4442b69bace3af77c9097ea1cf
Melissa Tapia <mtapia.dpl@gmail.com>
2022-04-25 14:51:29 -0500

    lists / abstraction start

--- abstraction4_1/Program.cs
+++ abstraction4_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace abstraction4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
